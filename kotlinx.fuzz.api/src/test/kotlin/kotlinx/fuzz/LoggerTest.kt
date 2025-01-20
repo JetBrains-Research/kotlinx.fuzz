d83c135ef9e3cfd34a2e4cce71ce2c75e26b0f09
FerrumBrain <lemur1416@gmail.com>
2025-01-20 18:15:02 +0100

    Fixed loggerTest

--- kotlinx.fuzz.api/src/test/kotlin/kotlinx/fuzz/LoggerTest.kt
+++ kotlinx.fuzz.api/src/test/kotlin/kotlinx/fuzz/LoggerTest.kt
@@ -1,13 +1,20 @@
 package kotlinx.fuzz
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import java.io.File
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class LoggerTest {
     val log = KotlinLogging.logger { }
 
     @Test
     fun loggerTest() {
+        val logFile = File("kotlinx.fuzz.log")
+        logFile.writeText("")
         log.debug { "Test" }
+        Thread.sleep(100) // We need to wait so that file updates
+        val newLines = logFile.readLines()
+        assertTrue { newLines.last().endsWith("[DEBUG][kotlinx.fuzz.LoggerTest] - Test") }
     }
 }
