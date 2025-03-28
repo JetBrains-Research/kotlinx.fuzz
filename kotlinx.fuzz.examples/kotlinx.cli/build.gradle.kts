32c2dd8b50035e86ad529ac1286dc82aa743d332
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-03-28 10:26:40 +0100

    use plan-jazzer 0.0.4

--- kotlinx.fuzz.examples/kotlinx.cli/build.gradle.kts
+++ kotlinx.fuzz.examples/kotlinx.cli/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation(libs.jazzer.api)
-    implementation(libs.jazzer.junit)
+    implementation(libs.plan.jazzer.api)
+    implementation(libs.plan.jazzer.junit)
     implementation(libs.kotlinx.cli)
     implementation(libs.commons.cli)
 }
